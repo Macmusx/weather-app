cac41083f8244f90359e45b5c8a55542abd4c6f2
LucianBarcan <luci.barcan@yahoo.com>
2023-08-08 20:55:30 +0300

    Enlarge important text

--- src/components/weather/TodayBrief.tsx
+++ src/components/weather/TodayBrief.tsx
@@ -7,7 +7,7 @@ export default function TodayBrief(props: { service: GlobalService }) {
 
     return (<div className="max-w-2xl rounded-2xl overflow-hidden shadow-lg m-5 sm:max-w-[30rem] bg-white">
         <>
-            <div className='flex justify-evenly'>
+            <div className='flex justify-evenly items-center'>
                 <div className='min-w-[8rem]'>
                     <img className="w-[5rem] m-auto mt-3"
                          src={require('../../images/weather/' + currentForecast.weatherIcon)}
@@ -15,20 +15,20 @@ export default function TodayBrief(props: { service: GlobalService }) {
                     <div className="text-center mt-4">{currentForecast.currentConditionMessage}</div>
                     <div className="font-bold text-4xl text-center">{currentForecast.temp}</div>
                     <div className="text-center">Feels like</div>
-                    <div className="text-center mb-2">{currentForecast.feelslike}</div>
+                    <div className="font-bold text-center text-2xl mb-2">{currentForecast.feelslike}</div>
                 </div>
 
                 <div
-                    className="m-0.5 min-h-[1em] w-px bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20"></div>
+                    className="h-[13rem] m-0.5 w-px bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20"></div>
 
                 <div className='mt-4 ml-3 mr-3'>
-                    <div className="font-bold text-xl text-center">{currentForecast.wind}</div>
+                    <div className="font-bold text-2xl text-center">{currentForecast.wind}</div>
                     <div className="text-center">Wind Speed</div>
-                    <div className="font-bold text-xl text-center">{currentForecast.pressure}</div>
+                    <div className="font-bold text-2xl text-center">{currentForecast.pressure}</div>
                     <div className="text-center">Pressure</div>
-                    <div className="font-bold text-xl text-center">{currentForecast.visibility}</div>
+                    <div className="font-bold text-2xl text-center">{currentForecast.visibility}</div>
                     <div className="text-center">Visibility</div>
-                    <div className="font-bold text-xl text-center">{currentForecast.precipitation}</div>
+                    <div className="font-bold text-2xl text-center">{currentForecast.precipitation}</div>
                     <div className="text-center mb-2">Precipitation</div>
                 </div>
 
