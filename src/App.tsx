294ce3b9a07dea037043fd32117b3d925e0442af
LucianBarcan <luci.barcan@yahoo.com>
2023-08-09 20:36:13 +0300

    Add Loading Spinner

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,11 @@ function App(props: { service: GlobalService }) {
         }
     }, []);
 
-    if (service.loading) return <div>Loading...</div>;
+    if (service.loading) return (
+        <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+    );
 
     return (
         <>
