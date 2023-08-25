6d7f773460df7c75dc4dce13aaaa7d783dfe9593
LucianBarcan <luci.barcan@yahoo.com>
2023-08-25 11:07:13 +0300

    Fix: Forecast no longer has AirQuality

--- src/classes/ForecastDayDetails.ts
+++ src/classes/ForecastDayDetails.ts
@@ -1,89 +1,87 @@
 import {Condition, ConditionDto} from "./Condition";
-import {AirQuality, AirQualityDto} from "./AirQuality";
+import {AirQualityDto} from "./AirQuality";
 import {GlobalService} from "./GlobalService";
 import {ForecastShared} from "./ForecastShared";
 
 export interface ForecastDayDetailsDto {
-    maxtemp_c: number,
-    maxtemp_f: number,
-    mintemp_c: number,
-    mintemp_f: number,
-    avgtemp_c: number,
-    avgtemp_f: number,
-    maxwind_mph: number,
-    maxwind_kph: number,
-    totalprecip_mm: number,
-    totalprecip_in: number,
-    totalsnow_cm: number,
-    avgvis_km: number,
-    avgvis_miles: number,
-    avghumidity: number,
-    daily_will_it_rain: number,
-    daily_chance_of_rain: string,
-    daily_will_it_snow: number,
-    daily_chance_of_snow: string,
-    condition: ConditionDto,
-    uv: number,
-    air_quality: AirQualityDto,
+  maxtemp_c: number,
+  maxtemp_f: number,
+  mintemp_c: number,
+  mintemp_f: number,
+  avgtemp_c: number,
+  avgtemp_f: number,
+  maxwind_mph: number,
+  maxwind_kph: number,
+  totalprecip_mm: number,
+  totalprecip_in: number,
+  totalsnow_cm: number,
+  avgvis_km: number,
+  avgvis_miles: number,
+  avghumidity: number,
+  daily_will_it_rain: number,
+  daily_chance_of_rain: string,
+  daily_will_it_snow: number,
+  daily_chance_of_snow: string,
+  condition: ConditionDto,
+  uv: number,
+  air_quality: AirQualityDto,
 }
 
 export class ForecastDayDetails extends ForecastShared {
-    private readonly _maxtemp_c: number;
-    private readonly _maxtemp_f: number;
-    private readonly _mintemp_c: number;
-    private readonly _mintemp_f: number;
-    private readonly _avgtemp_c: number;
-    private readonly _avgtemp_f: number;
-    private readonly _maxwind_mph: number;
-    private readonly _maxwind_kph: number;
-    private readonly _totalprecip_mm: number;
-    private readonly _totalprecip_in: number;
-    private readonly _totalsnow_cm: number;
-    private readonly _avgvis_km: number;
-    private readonly _avgvis_miles: number;
-    private readonly _avghumidity: number;
-    private readonly _daily_will_it_rain: number;
-    private readonly _daily_chance_of_rain: string;
-    private readonly _daily_will_it_snow: number;
-    private readonly _daily_chance_of_snow: string;
-    private readonly _condition: Condition;
-    private readonly _uv: number;
-    private readonly _air_quality: AirQuality;
+  private readonly _maxtemp_c: number;
+  private readonly _maxtemp_f: number;
+  private readonly _mintemp_c: number;
+  private readonly _mintemp_f: number;
+  private readonly _avgtemp_c: number;
+  private readonly _avgtemp_f: number;
+  private readonly _maxwind_mph: number;
+  private readonly _maxwind_kph: number;
+  private readonly _totalprecip_mm: number;
+  private readonly _totalprecip_in: number;
+  private readonly _totalsnow_cm: number;
+  private readonly _avgvis_km: number;
+  private readonly _avgvis_miles: number;
+  private readonly _avghumidity: number;
+  private readonly _daily_will_it_rain: number;
+  private readonly _daily_chance_of_rain: string;
+  private readonly _daily_will_it_snow: number;
+  private readonly _daily_chance_of_snow: string;
+  private readonly _condition: Condition;
+  private readonly _uv: number;
 
-    constructor(dto: ForecastDayDetailsDto, engine: GlobalService) {
-        super(engine);
-        this._maxtemp_c = dto.maxtemp_c;
-        this._maxtemp_f = dto.maxtemp_f;
-        this._mintemp_c = dto.mintemp_c;
-        this._mintemp_f = dto.mintemp_f;
-        this._avgtemp_c = dto.avgtemp_c;
-        this._avgtemp_f = dto.avgtemp_f;
-        this._maxwind_mph = dto.maxwind_mph;
-        this._maxwind_kph = dto.maxwind_kph;
-        this._totalprecip_mm = dto.totalprecip_mm;
-        this._totalprecip_in = dto.totalprecip_in;
-        this._totalsnow_cm = dto.totalsnow_cm;
-        this._avgvis_km = dto.avgvis_km;
-        this._avgvis_miles = dto.avgvis_miles;
-        this._avghumidity = dto.avghumidity;
-        this._daily_will_it_rain = dto.daily_will_it_rain;
-        this._daily_chance_of_rain = dto.daily_chance_of_rain;
-        this._daily_will_it_snow = dto.daily_will_it_snow;
-        this._daily_chance_of_snow = dto.daily_chance_of_snow;
-        this._condition = new Condition(dto.condition, this.engine);
-        this._uv = dto.uv;
-        this._air_quality = new AirQuality(dto.air_quality, this.engine);
-    }
+  constructor(dto: ForecastDayDetailsDto, engine: GlobalService) {
+    super(engine);
+    this._maxtemp_c = dto.maxtemp_c;
+    this._maxtemp_f = dto.maxtemp_f;
+    this._mintemp_c = dto.mintemp_c;
+    this._mintemp_f = dto.mintemp_f;
+    this._avgtemp_c = dto.avgtemp_c;
+    this._avgtemp_f = dto.avgtemp_f;
+    this._maxwind_mph = dto.maxwind_mph;
+    this._maxwind_kph = dto.maxwind_kph;
+    this._totalprecip_mm = dto.totalprecip_mm;
+    this._totalprecip_in = dto.totalprecip_in;
+    this._totalsnow_cm = dto.totalsnow_cm;
+    this._avgvis_km = dto.avgvis_km;
+    this._avgvis_miles = dto.avgvis_miles;
+    this._avghumidity = dto.avghumidity;
+    this._daily_will_it_rain = dto.daily_will_it_rain;
+    this._daily_chance_of_rain = dto.daily_chance_of_rain;
+    this._daily_will_it_snow = dto.daily_will_it_snow;
+    this._daily_chance_of_snow = dto.daily_chance_of_snow;
+    this._condition = new Condition(dto.condition, this.engine);
+    this._uv = dto.uv;
+  }
 
-    get weatherIcon() {
-        return super.getWeatherIcon(this._condition.code, true);
-    }
+  get weatherIcon() {
+    return super.getWeatherIcon(this._condition.code, true);
+  }
 
-    get temp() {
-        return super.getTemp(this._avgtemp_c, this._avgtemp_f);
-    }
+  get temp() {
+    return super.getTemp(this._avgtemp_c, this._avgtemp_f);
+  }
 
-    get currentConditionMessage() {
-        return this._condition.text;
-    }
+  get currentConditionMessage() {
+    return this._condition.text;
+  }
 }
--- src/classes/ForecastDayHour.ts
+++ src/classes/ForecastDayHour.ts
@@ -1,114 +1,112 @@
 import {Condition, ConditionDto} from "./Condition";
-import {AirQuality, AirQualityDto} from "./AirQuality";
+import {AirQualityDto} from "./AirQuality";
 import {GlobalService} from "./GlobalService";
 
 export interface ForecastDayHourDto {
-    time_epoch: number,
-    time: string,
-    temp_c: number,
-    temp_f: number,
-    is_day: 0 | 1,
-    condition: ConditionDto,
-    wind_mph: number,
-    wind_kph: number,
-    wind_degree: number,
-    wind_dir: string,
-    pressure_mb: number,
-    pressure_in: number,
-    precip_mm: number,
-    precip_in: number,
-    humidity: number,
-    cloud: number,
-    feelslike_c: number,
-    feelslike_f: number,
-    windchill_c: number,
-    windchill_f: number,
-    heatindex_c: number,
-    heatindex_f: number,
-    dewpoint_c: number,
-    dewpoint_f: number,
-    will_it_rain: 0 | 1,
-    chance_of_rain: string,
-    will_it_snow: 0 | 1,
-    chance_of_snow: string,
-    vis_km: number,
-    vis_miles: number,
-    gust_mph: number,
-    gust_kph: number,
-    uv: number,
-    air_quality: AirQualityDto
+  time_epoch: number,
+  time: string,
+  temp_c: number,
+  temp_f: number,
+  is_day: 0 | 1,
+  condition: ConditionDto,
+  wind_mph: number,
+  wind_kph: number,
+  wind_degree: number,
+  wind_dir: string,
+  pressure_mb: number,
+  pressure_in: number,
+  precip_mm: number,
+  precip_in: number,
+  humidity: number,
+  cloud: number,
+  feelslike_c: number,
+  feelslike_f: number,
+  windchill_c: number,
+  windchill_f: number,
+  heatindex_c: number,
+  heatindex_f: number,
+  dewpoint_c: number,
+  dewpoint_f: number,
+  will_it_rain: 0 | 1,
+  chance_of_rain: string,
+  will_it_snow: 0 | 1,
+  chance_of_snow: string,
+  vis_km: number,
+  vis_miles: number,
+  gust_mph: number,
+  gust_kph: number,
+  uv: number,
+  air_quality: AirQualityDto
 }
 
 export class ForecastDayHour {
-    private readonly _time_epoch: number;
-    private readonly _time: string;
-    private readonly _temp_c: number;
-    private readonly _temp_f: number;
-    private readonly _is_day: 0 | 1;
-    private readonly _condition: Condition;
-    private readonly _wind_mph: number;
-    private readonly _wind_kph: number;
-    private readonly _wind_degree: number;
-    private readonly _wind_dir: string;
-    private readonly _pressure_mb: number;
-    private readonly _pressure_in: number;
-    private readonly _precip_mm: number;
-    private readonly _precip_in: number;
-    private readonly _humidity: number;
-    private readonly _cloud: number;
-    private readonly _feelslike_c: number;
-    private readonly _feelslike_f: number;
-    private readonly _windchill_c: number;
-    private readonly _windchill_f: number;
-    private readonly _heatindex_c: number;
-    private readonly _heatindex_f: number;
-    private readonly _dewpoint_c: number;
-    private readonly _dewpoint_f: number;
-    private readonly _will_it_rain: 0 | 1;
-    private readonly _chance_of_rain: string;
-    private readonly _will_it_snow: 0 | 1;
-    private readonly _chance_of_snow: string;
-    private readonly _vis_km: number;
-    private readonly _vis_miles: number;
-    private readonly _gust_mph: number;
-    private readonly _gust_kph: number;
-    private readonly _uv: number;
-    private readonly _air_quality: AirQuality;
+  private readonly _time_epoch: number;
+  private readonly _time: string;
+  private readonly _temp_c: number;
+  private readonly _temp_f: number;
+  private readonly _is_day: 0 | 1;
+  private readonly _condition: Condition;
+  private readonly _wind_mph: number;
+  private readonly _wind_kph: number;
+  private readonly _wind_degree: number;
+  private readonly _wind_dir: string;
+  private readonly _pressure_mb: number;
+  private readonly _pressure_in: number;
+  private readonly _precip_mm: number;
+  private readonly _precip_in: number;
+  private readonly _humidity: number;
+  private readonly _cloud: number;
+  private readonly _feelslike_c: number;
+  private readonly _feelslike_f: number;
+  private readonly _windchill_c: number;
+  private readonly _windchill_f: number;
+  private readonly _heatindex_c: number;
+  private readonly _heatindex_f: number;
+  private readonly _dewpoint_c: number;
+  private readonly _dewpoint_f: number;
+  private readonly _will_it_rain: 0 | 1;
+  private readonly _chance_of_rain: string;
+  private readonly _will_it_snow: 0 | 1;
+  private readonly _chance_of_snow: string;
+  private readonly _vis_km: number;
+  private readonly _vis_miles: number;
+  private readonly _gust_mph: number;
+  private readonly _gust_kph: number;
+  private readonly _uv: number;
 
-    constructor(dto: ForecastDayHourDto, private readonly engine: GlobalService) {
-        this._time_epoch = dto.time_epoch;
-        this._time = dto.time;
-        this._temp_c = dto.temp_c;
-        this._temp_f = dto.temp_f;
-        this._is_day = dto.is_day;
-        this._condition = new Condition(dto.condition, this.engine);
-        this._wind_mph = dto.wind_mph;
-        this._wind_kph = dto.wind_kph;
-        this._wind_degree = dto.wind_degree;
-        this._wind_dir = dto.wind_dir;
-        this._pressure_mb = dto.pressure_mb;
-        this._pressure_in = dto.pressure_in;
-        this._precip_mm = dto.precip_mm;
-        this._precip_in = dto.precip_in;
-        this._humidity = dto.humidity;
-        this._cloud = dto.cloud;
-        this._feelslike_c = dto.feelslike_c;
-        this._feelslike_f = dto.feelslike_f;
-        this._windchill_c = dto.windchill_c;
-        this._windchill_f = dto.windchill_f;
-        this._heatindex_c = dto.heatindex_c;
-        this._heatindex_f = dto.heatindex_f;
-        this._dewpoint_c = dto.dewpoint_c;
-        this._dewpoint_f = dto.dewpoint_f;
-        this._will_it_rain = dto.will_it_rain;
-        this._chance_of_rain = dto.chance_of_rain;
-        this._will_it_snow = dto.will_it_snow;
-        this._chance_of_snow = dto.chance_of_snow;
-        this._vis_km = dto.vis_km;
-        this._vis_miles = dto.vis_miles;
-        this._gust_mph = dto.gust_mph;
-        this._gust_kph = dto.gust_kph;
-        this._uv = dto.uv;
-        this._air_quality = new AirQuality(dto.air_quality, this.engine);
-    }
+  constructor(dto: ForecastDayHourDto, private readonly engine: GlobalService) {
+    this._time_epoch = dto.time_epoch;
+    this._time = dto.time;
+    this._temp_c = dto.temp_c;
+    this._temp_f = dto.temp_f;
+    this._is_day = dto.is_day;
+    this._condition = new Condition(dto.condition, this.engine);
+    this._wind_mph = dto.wind_mph;
+    this._wind_kph = dto.wind_kph;
+    this._wind_degree = dto.wind_degree;
+    this._wind_dir = dto.wind_dir;
+    this._pressure_mb = dto.pressure_mb;
+    this._pressure_in = dto.pressure_in;
+    this._precip_mm = dto.precip_mm;
+    this._precip_in = dto.precip_in;
+    this._humidity = dto.humidity;
+    this._cloud = dto.cloud;
+    this._feelslike_c = dto.feelslike_c;
+    this._feelslike_f = dto.feelslike_f;
+    this._windchill_c = dto.windchill_c;
+    this._windchill_f = dto.windchill_f;
+    this._heatindex_c = dto.heatindex_c;
+    this._heatindex_f = dto.heatindex_f;
+    this._dewpoint_c = dto.dewpoint_c;
+    this._dewpoint_f = dto.dewpoint_f;
+    this._will_it_rain = dto.will_it_rain;
+    this._chance_of_rain = dto.chance_of_rain;
+    this._will_it_snow = dto.will_it_snow;
+    this._chance_of_snow = dto.chance_of_snow;
+    this._vis_km = dto.vis_km;
+    this._vis_miles = dto.vis_miles;
+    this._gust_mph = dto.gust_mph;
+    this._gust_kph = dto.gust_kph;
+    this._uv = dto.uv;
+  }
 }
